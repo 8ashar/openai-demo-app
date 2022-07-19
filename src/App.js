78a59f5bfc4e6e54fc126fa4ce777e47ea3a3d05
Bashar Eid <8ashareid@gmail.com>
2022-07-19 12:21:23 -0500

    implemented feature to update input textfield placeholder based on selected input language

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function App() {
 
   const handleInputChange = (event) => {
     setInputLang(event.target.value)
+    setInputText('');
+    document.getElementById("input").value = "";
+    document.getElementById("output".value="");
   };
 
   const handleInputText = (event) =>{
@@ -27,6 +30,92 @@ function App() {
 
   const handleOutputChange = (event) => {
     setOutputLang(event.target.value);
+  };
+
+  const handlePython = () => {
+    const text = `def find max(nums):
+    max = list(0)
+    
+    for num in nums:
+        if num > max:
+            max = num
+          
+    return max
+      
+numbers = [0, 5, 7, 88, 14]
+print(max(list))`;
+
+    document.getElementById("input").placeholder=text
+  };
+
+  const handleCobol = () => {
+    const text = `Sample Cobol`;
+
+    document.getElementById("input").placeholder=text;
+  };
+
+  const handleJavaScript = () => {
+    const text = `function findMax(nums) {
+    let max = nums[0];
+
+    for (var i = 0; i < nums.length; i++)
+        if (nums[i] > max) max = nums[i];
+
+    return max;
+}
+
+const numbers = [0, 5, 7, 88, 14];
+console.log(findMax(numbers));`
+
+  document.getElementById("input").placeholder=text;
+  };
+
+  const handleJava = () => {
+    const text = `public static int findMax(int[] nums) {
+    int max = nums[0];
+
+    for (int num : nums)
+        if (num > max) max = num;
+
+    return max;
+}
+
+public static void main(String[] args) {
+    int[] numbers = [0, 5, 7, 88, 14];
+    System.out.println(findMax(numbers))
+}`
+
+    document.getElementById("input").placeholder=text;
+  };
+
+  const handleC = () => {
+    const text = `int findMax(int nums[]) {
+    int i, max = arr[0];
+
+    for (i = 0; i < sizeof(nums) / sizeof(max); i++)
+        if (nums[i] > max) max = nums[i];
+
+    return max;
+}
+
+int main() {
+    int numbers[] = [0, 5, 7, 88, 14];
+    printf("%d", findMax(numbers));
+    return 0;
+}`
+
+    document.getElementById("input").placeholder=text;
+  };
+
+  const handleSql = () => {
+    const text = `SELECT MIN(Price) AS SmallestPrice FROM Products;`;
+
+    document.getElementById("input").placeholder=text;
+  };
+
+  const handleEnglish = () => {
+    const text = `find the maximum of an array of numbers`
+    document.getElementById("input").placeholder=text;
   };
 
   return (
@@ -50,24 +139,25 @@ function App() {
                     label="InputLang"
                     onChange={handleInputChange}
                   >
-                    <MenuItem value={"Python"}>Python</MenuItem>
-                    <MenuItem value={"Cobol"}>COBOL</MenuItem>
-                    <MenuItem value={"JavaScript"}>JS</MenuItem>
-                    <MenuItem value={"Java"}>Java</MenuItem>
-                    <MenuItem value={"C"}>C</MenuItem>
-                    <MenuItem value={"C++"}>C++</MenuItem>
-                    <MenuItem value={"SQL"}>SQL</MenuItem>
-                    <MenuItem value={"English"}>English</MenuItem>
+                    <MenuItem onClick={handlePython} value={"Python"}>Python</MenuItem>
+                    <MenuItem onClick={handleCobol} value={"Cobol"}>COBOL</MenuItem>
+                    <MenuItem onClick={handleJavaScript} value={"JavaScript"}>JS</MenuItem>
+                    <MenuItem onClick={handleJava} value={"Java"}>Java</MenuItem>
+                    <MenuItem onClick={handleC} value={"C"}>C</MenuItem>
+                    <MenuItem onClick={handleSql} value={"SQL"}>SQL</MenuItem>
+                    <MenuItem onClick={handleEnglish} value={"English"}>English</MenuItem>
                   </Select>
                 </FormControl>
               </Box>
             </Grid>
 
-            <Grid item xs={12} sx={{height: 300, paddingTop: '2.5%'}}>
+            <Grid item xs={12} sx={{height: 400, paddingTop: '2.5%'}}>
               <TextField
+                placeholder=''
+                label='Enter your source here:'
                 id="input"
                 sx={{width: '90%'}}
-                multiline rows={11}
+                multiline rows={15}
                 onChange={handleInputText}
                 InputProps={{
                   endAdornment: (
@@ -77,6 +167,7 @@ function App() {
                         onClick={() => {
                           setInputText('');
                           document.getElementById("input").value = "";
+                          document.getElementById("output").value="";
                         }}
                       >
                         <ClearIcon />
@@ -143,12 +234,6 @@ function App() {
                     </MenuItem>
 
                     <MenuItem
-                      disabled={inputLang === "C++" || inputLang === "SQL"}
-                      value={"C++"}>
-                        C++
-                    </MenuItem>
-
-                    <MenuItem
                       disabled={!(inputLang === "English")}
                       value={"SQL"}>
                       SQL
@@ -163,12 +248,12 @@ function App() {
               </Box>
             </Grid>
  
-            <Grid item xs={12} sx={{height: 300, paddingTop: '2.5%'}}>
+            <Grid item xs={12} sx={{height: 400, paddingTop: '2.5%'}}>
               <TextField
                 disabled
                 id="output"
-                sx={{width: '90%', input: {color: 'black'}}}
-                multiline rows={11}
+                sx={{width: '90%'}}
+                multiline rows={15}
                 InputProps={{
                   endAdornment: (
                     <InputAdornment>
